720f07b5f68807c5b177a232c7756ee1e0b4d2c9
Paul Calzado <pafcalzado@gmail.com>
2023-11-07 13:22:58 +0800

    Fixes bug appending number to result from previous operation

--- app.js
+++ app.js
@@ -8,25 +8,25 @@ const ERROR_MESSAGE = "[SNARKILY]: CAN'T DIVIDE BY 0";
 const operationArr = [];
 
 const keyCodeObj = {
-	Numpad0: '0',
-	Numpad1: '1',
-	Numpad2: '2',
-	Numpad3: '3',
-	Numpad4: '4',
-	Numpad5: '5',
-	Numpad6: '6',
-	Numpad7: '7',
-	Numpad8: '8',
-	Numpad9: '9',
-	NumpadDivide: '/',
-	NumpadMultiply: '*',
-	NumpadSubtract: '-',
-	NumpadAdd: '+',
-	NumpadEnter: '=',
-	NumpadDecimal: '.',
-	Minus: 'posneg',
-	Backspace: 'erase',
-	Delete: 'clear',
+  Numpad0: '0',
+  Numpad1: '1',
+  Numpad2: '2',
+  Numpad3: '3',
+  Numpad4: '4',
+  Numpad5: '5',
+  Numpad6: '6',
+  Numpad7: '7',
+  Numpad8: '8',
+  Numpad9: '9',
+  NumpadDivide: '/',
+  NumpadMultiply: '*',
+  NumpadSubtract: '-',
+  NumpadAdd: '+',
+  NumpadEnter: '=',
+  NumpadDecimal: '.',
+  Minus: 'posneg',
+  Backspace: 'erase',
+  Delete: 'clear',
 };
 
 current.textContent = operandStr;
@@ -35,223 +35,226 @@ window.addEventListener('keydown', (event) => keyDownHandler(event));
 window.addEventListener('keyup', (event) => keyUpHandler(event));
 
 [...buttons].forEach((button) =>
-	button.addEventListener('click', (event) => clickHandler(event.target))
+  button.addEventListener('click', (event) => clickHandler(event.target))
 );
 
 function clickHandler(data) {
-	let dataInput = data.dataset['input'];
-	let dataType = data.classList[1];
+  let dataInput = data.dataset['input'];
+  let dataType = data.classList[1];
 
-	if (current.textContent === ERROR_MESSAGE) {
-		reset();
-	}
+  if (
+    current.textContent === ERROR_MESSAGE ||
+    current.textContent.includes('=')
+  ) {
+    reset();
+  }
 
-	switch (dataType) {
-		case 'operand':
-			operandHandler(dataInput);
-			break;
-		case 'operator':
-			operatorHandler(dataInput);
-			break;
-		case 'evaluate':
-			evaluate();
-			break;
-		case 'function':
-			handleFunctions(dataInput);
-			break;
-		default:
-			break;
-	}
+  switch (dataType) {
+    case 'operand':
+      operandHandler(dataInput);
+      break;
+    case 'operator':
+      operatorHandler(dataInput);
+      break;
+    case 'evaluate':
+      evaluate();
+      break;
+    case 'function':
+      handleFunctions(dataInput);
+      break;
+    default:
+      break;
+  }
 
-	updateDisplay();
+  updateDisplay();
 }
 
 function keyDownHandler(data) {
-	const dataInput = keyCodeObj[data.code];
+  const dataInput = keyCodeObj[data.code];
 
-	if (!dataInput) {
-		return;
-	}
+  if (!dataInput) {
+    return;
+  }
 
-	const [button] = [...buttons].filter(
-		(button) => button.dataset['input'] === dataInput
-	);
-	const dataType = button ? button.classList[1] : null;
+  const [button] = [...buttons].filter(
+    (button) => button.dataset['input'] === dataInput
+  );
+  const dataType = button ? button.classList[1] : null;
 
-	button.classList.add('active');
+  button.classList.add('active');
 
-	if (current.textContent === ERROR_MESSAGE) {
-		reset();
-	}
+  if (current.textContent === ERROR_MESSAGE) {
+    reset();
+  }
 
-	switch (dataType) {
-		case 'operand':
-			operandHandler(dataInput);
-			break;
-		case 'operator':
-			operatorHandler(dataInput);
-			break;
-		case 'evaluate':
-			evaluate();
-			break;
-		case 'function':
-			handleFunctions(dataInput);
-			break;
-		default:
-			break;
-	}
+  switch (dataType) {
+    case 'operand':
+      operandHandler(dataInput);
+      break;
+    case 'operator':
+      operatorHandler(dataInput);
+      break;
+    case 'evaluate':
+      evaluate();
+      break;
+    case 'function':
+      handleFunctions(dataInput);
+      break;
+    default:
+      break;
+  }
 
-	updateDisplay();
+  updateDisplay();
 }
 
 function keyUpHandler(data) {
-	const dataInput = keyCodeObj[data.code];
+  const dataInput = keyCodeObj[data.code];
 
-	if (!dataInput) return;
+  if (!dataInput) return;
 
-	const [button] = [...buttons].filter(
-		(button) => button.dataset['input'] === dataInput
-	);
+  const [button] = [...buttons].filter(
+    (button) => button.dataset['input'] === dataInput
+  );
 
-	button.classList.remove('active');
+  button.classList.remove('active');
 }
 
 function operandHandler(input) {
-	if (operandStr === '0') {
-		if (input === '0') {
-			operandStr = '0';
-		} else if (input !== '0') {
-			operandStr = input;
-		} else if (input === '.') {
-			operandStr = '0.';
-		}
-	} else if (input === '.') {
-		decimalHandler(input);
-	} else {
-		operandStr += input;
-	}
+  if (operandStr === '0') {
+    if (input === '0') {
+      operandStr = '0';
+    } else if (input !== '0') {
+      operandStr = input;
+    } else if (input === '.') {
+      operandStr = '0.';
+    }
+  } else if (input === '.') {
+    decimalHandler(input);
+  } else {
+    operandStr += input;
+  }
 }
 
 function decimalHandler(input) {
-	if (operandStr.includes('.')) {
-		return;
-	} else {
-		operandStr += input;
-	}
+  if (operandStr.includes('.')) {
+    return;
+  } else {
+    operandStr += input;
+  }
 }
 
 function operatorHandler(input) {
-	if (operationArr.length === 1) {
-		operandStr = operationArr[0].toString();
-		operationArr.push(input);
-	} else {
-		operationArr.push(Number(operandStr), input);
-	}
-	operandStr = '';
+  if (operationArr.length === 1) {
+    operandStr = operationArr[0].toString();
+    operationArr.push(input);
+  } else {
+    operationArr.push(Number(operandStr), input);
+  }
+  operandStr = '';
 }
 
 function evaluate() {
-	let result = 0;
+  let result = 0;
 
-	if (operandStr.length && !operationArr.length) {
-		return;
-	}
+  if (operandStr.length && !operationArr.length) {
+    return;
+  }
 
-	operationArr.push(Number(operandStr));
-	historyStr = operationArr.join('');
-	result = operation(operationArr);
-	operandStr = result.toString();
+  operationArr.push(Number(operandStr));
+  historyStr = operationArr.join('');
+  result = operation(operationArr);
+  operandStr = result.toString();
 }
 
 function operation(arr) {
-	let result = 0;
-	while (arr.length !== 1) {
-		const firstThreeIdx = arr.slice(0, 3);
-		result = Math.round(operate(...firstThreeIdx) * 1000) / 1000;
-		arr.splice(0, 3, Math.round(result * 1000) / 1000);
-	}
+  let result = 0;
+  while (arr.length !== 1) {
+    const firstThreeIdx = arr.slice(0, 3);
+    result = Math.round(operate(...firstThreeIdx) * 1000) / 1000;
+    arr.splice(0, 3, Math.round(result * 1000) / 1000);
+  }
 
-	return result;
+  return result;
 }
 
 function operate(num1, operator, num2) {
-	let result = 0;
+  let result = 0;
 
-	switch (operator) {
-		case '+':
-			result = num1 + num2;
-			break;
-		case '-':
-			result = num1 - num2;
-			break;
-		case '*':
-			result = num1 * num2;
-			break;
-		case '/':
-			result = num1 / num2;
-			break;
-		default:
-			break;
-	}
+  switch (operator) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      result = num1 / num2;
+      break;
+    default:
+      break;
+  }
 
-	return result;
+  return result;
 }
 
 function reset() {
-	historyStr = '';
-	operandStr = '0';
-	operationArr.length = 0;
-	history.textContent = historyStr;
-	current.textContent = operandStr;
-	current.style.fontSize = '36px';
+  historyStr = '';
+  operandStr = '0';
+  operationArr.length = 0;
+  history.textContent = historyStr;
+  current.textContent = operandStr;
+  current.style.fontSize = '36px';
 }
 
 function handleFunctions(funcType) {
-	switch (funcType) {
-		case 'clear':
-			reset();
-			break;
-		case 'posneg':
-			toggleNegative();
-			break;
-		case 'erase':
-			handleErase();
-			break;
-		default:
-			break;
-	}
+  switch (funcType) {
+    case 'clear':
+      reset();
+      break;
+    case 'posneg':
+      toggleNegative();
+      break;
+    case 'erase':
+      handleErase();
+      break;
+    default:
+      break;
+  }
 }
 
 function toggleNegative() {
-	if (operandStr === '0' || !operandStr.length) {
-		return;
-	}
+  if (operandStr === '0' || !operandStr.length) {
+    return;
+  }
 
-	if (operationArr.length === 1) {
-		operandStr = '-' + operationArr[0].toString();
-	} else if (!operandStr.includes('-')) {
-		operandStr = '-' + operandStr;
-	} else {
-		operandStr = operandStr.substring(1);
-	}
+  if (operationArr.length === 1) {
+    operandStr = '-' + operationArr[0].toString();
+  } else if (!operandStr.includes('-')) {
+    operandStr = '-' + operandStr;
+  } else {
+    operandStr = operandStr.substring(1);
+  }
 }
 
 function handleErase() {
-	if (operandStr === '0' || operationArr.length === 1) {
-		return;
-	} else {
-		operandStr = operandStr.substring(0, operandStr.length - 1);
-	}
+  if (operandStr === '0' || operationArr.length === 1) {
+    return;
+  } else {
+    operandStr = operandStr.substring(0, operandStr.length - 1);
+  }
 }
 
 function updateDisplay() {
-	if (operandStr === 'Infinity') {
-		current.textContent = ERROR_MESSAGE;
-		current.style.fontSize = '20px';
-	} else if (operationArr.length === 1) {
-		current.textContent = `=${operandStr}`;
-	} else {
-		current.textContent = operationArr.join('') + operandStr;
-	}
-	history.textContent = historyStr;
+  if (operandStr === 'Infinity') {
+    current.textContent = ERROR_MESSAGE;
+    current.style.fontSize = '20px';
+  } else if (operationArr.length === 1) {
+    current.textContent = `=${operandStr}`;
+  } else {
+    current.textContent = operationArr.join('') + operandStr;
+  }
+  history.textContent = historyStr;
 }
